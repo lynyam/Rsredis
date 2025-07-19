2c9db34886368632644183ec9363cf294b520c00
léon yawovi NYAMETSO <leonyawovinyametso@Mac-mini-de-leon.local>
2025-07-19 08:26:19 +0200

    add true to PrintWriter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
-          PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+          PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
           out.println("+PONG\r\n");
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
